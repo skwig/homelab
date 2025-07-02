7baca6c1fd293122bd8b033fd1459e7486418212
skwig <16136203+skwig@users.noreply.github.com>
2025-07-02 16:11:56 +0200

    feat(iac): added terraform

---
+++ iac/.terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azuread" {
+  version = "3.4.0"
+  hashes = [
+    "h1:rfO7hSYJLdpff/s2iuooHtxNacwKq5n03IwvVQ+xbSI=",
+    "zh:035a6d6e6aa7f117969702873c27344ec4ddd88f676cebc1088316fb26d5c95a",
+    "zh:11f86935174d8223699cae00b3a705ded1d75a4efb6d4723d3788f5446e1eaa5",
+    "zh:16d52b5bf8eefa98cd2793122be0c5a7b41767caedbd8a08786aeefb3d0c6856",
+    "zh:1c3e89cf19118fc07d7b04257251fc9897e722c16e0a0df7b07fcd261f8c12e7",
+    "zh:2fe201c7a1c17279f7674c160861296015d9b9d120de598999d169398ce285c9",
+    "zh:37bb91dff5b751f0c86a02a12980bdb5935d2ca6cdd249d9eef7eca619f628c0",
+    "zh:7533a35300e411893a024f858e722e50107dfd7212236d396ebf2ca2b13b7bcc",
+    "zh:88a95b2cb606439ae2f60ebe63a800580e232e94bc1b02ac7d25d25be10cb511",
+    "zh:c7b138b6bc34d8a1eff91742b38bce1718d9c50c343393fdfc918bef022ed74f",
+    "zh:cab09fda45b8a9a9896aedb22f5829745b7e9a01abb8077696bccb170fb01b5f",
+    "zh:cc4a29f074f1cc25f3abd3a41444f68307f3eb08c4d5f79f60a012b632c1ea05",
+    "zh:e30e9fe8e04271431cb730a1a888b6da5afeae385e2e53ff7b4114066c1250db",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "4.35.0"
+  constraints = "4.35.0"
+  hashes = [
+    "h1:3zJsyLWItriZDhtx6kBkoy9UPcA9l5G4PKi4ZYxhsnA=",
+    "zh:05a7fac1291517527d272dd2e22a8b3223775d399dc608c8250435e63d437386",
+    "zh:3519dae43473cb5a82c026d664572859e2fc886fe2a54ecc59007186ef4aa166",
+    "zh:57cd9d04334d296a9af3aa54d776bc3b4709ecf5d511fee94dc7420a547364ab",
+    "zh:7d57cbd44ebf959fc496743b4e3587a51dd7546a687bab404dc3f5899d3ef764",
+    "zh:8c1e722c84aa8d769cdef0be933d6f096ff0ec8224eaf1ae2c7bdb93b0365c6f",
+    "zh:9519f72c937003ecc97a0a5a60c0ad1063dafa3d431cf04067bdc4b40ebfd023",
+    "zh:9bf108a2840ecfb3d2e76b1ac59d0d9bed418b1b28df8c2c5dd09e34cb1e3699",
+    "zh:b870884accad83c437a12a577933f378a21bb6663a6034a3fb219b4c1f4b9188",
+    "zh:cf3292dd535133afe16614ac84241065485d581ab67b4abf857db5bfbba7109e",
+    "zh:d48aa50227ca3701a16d2818b5d059c5f236344e3a90162fb930ecc6f136c9b0",
+    "zh:e63733db32189c1e849b5dbf7c10dd852f48ed4186c8af81240ee643d2df3425",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
---
+++ iac/main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.4.0"
+    }
+  }
+
+  backend "azurerm" {
+    tenant_id            = "b3033e23-4e79-48f8-a7b4-062fd451d4b2"
+    subscription_id      = "a9835b1f-dc51-4b91-83b1-436fc54726b5"
+    resource_group_name  = "skwig-homelab-rg-euw-prd"
+    storage_account_name = "skwighomelabsteuwprd"
+    container_name       = "iac"
+    key                  = "prd.terraform.tfstate"
+  }
+}
+
+locals {
+  tenant_id       = "b3033e23-4e79-48f8-a7b4-062fd451d4b2"
+  subscription_id = "a9835b1f-dc51-4b91-83b1-436fc54726b5"
+}
+
+provider "azurerm" {
+  subscription_id = local.subscription_id
+  features {}
+}
+
+provider "azuread" {
+}
+
+resource "azurerm_resource_group" "homelab_resource_group" {
+  name     = "skwig-homelab-rg-euw-prd"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "homelab_storage_account" {
+  name                     = "skwighomelabsteuwprd"
+  resource_group_name      = azurerm_resource_group.homelab_resource_group.name
+  location                 = azurerm_resource_group.homelab_resource_group.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "homelab_container_iac" {
+  name                  = "iac"
+  storage_account_id    = azurerm_storage_account.homelab_storage_account.id
+  container_access_type = "private"
+}
+
+resource "azuread_application" "homelab_application" {
+  display_name = "Skwig's homelab"
+}
+
+resource "azuread_service_principal" "homelab_service_principal" {
+  client_id = azuread_application.homelab_application.client_id
+}
+
+resource "azuread_service_principal_password" "homelab" {
+  service_principal_id = azuread_service_principal.homelab_service_principal.id
+}
+
+resource "azurerm_key_vault" "homelab_key_vault" {
+  name                      = "skwig-homelab-kv-euw-prd"
+  location                  = azurerm_resource_group.homelab_resource_group.location
+  resource_group_name       = azurerm_resource_group.homelab_resource_group.name
+  tenant_id                 = local.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+}
+
+resource "azurerm_role_assignment" "homelab_service_principal_key_vaultcontributor" {
+  scope                = azurerm_key_vault.homelab_key_vault.id
+  principal_id         = azuread_service_principal.homelab_service_principal.object_id
+  role_definition_name = "Key Vault Secrets User"
+}
+
+output "client_id" {
+  value = azuread_application.homelab_application.client_id
+}
+
+output "client_secret" {
+  value     = azuread_service_principal_password.homelab.value
+  sensitive = true
+}
+
+output "tenant_id" {
+  value = local.tenant_id
+}
+
+output "vault_uri" {
+  value = azurerm_key_vault.homelab_key_vault.vault_uri
+}
